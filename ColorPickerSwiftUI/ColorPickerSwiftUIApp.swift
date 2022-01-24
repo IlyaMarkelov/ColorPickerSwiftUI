9f566c8aeb349196024cf3996516f9d8dddaf63d
IlyaMarkelov <marckellow@icloud.com>
2022-01-24 22:03:07 +0300

    Initial Commit

---
+++ ColorPickerSwiftUI/ColorPickerSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  ColorPickerSwiftUIApp.swift
+//  ColorPickerSwiftUI
+//
+//  Created by Илья Маркелов on 24.01.2022.
+//
+
+import SwiftUI
+
+@main
+struct ColorPickerSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
